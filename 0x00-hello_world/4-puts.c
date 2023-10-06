d49481610309e851313625505acac19956893dd2
bossambani <123232647+bossambani@users.noreply.github.com>
2023-10-06 06:01:53 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - using puts function
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
