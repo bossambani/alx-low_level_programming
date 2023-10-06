57a30e60dbd3db2ef6edb4837d639a3d72564ef9
bossambani <123232647+bossambani@users.noreply.github.com>
2023-10-06 06:00:33 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
+/**
+ * main - using printf function
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
-	/**using printf function
-	 * instead of puts
-	 */
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
-
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
 }
