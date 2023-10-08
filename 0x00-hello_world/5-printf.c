3a673073fa2f030d164ff11711011d0694f90de7
bossambani <123232647+bossambani@users.noreply.github.com>
2023-10-08 19:12:18 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - using printf function
- *
- * Return: Always 0 (Success)
+ * main - using printf functions
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
