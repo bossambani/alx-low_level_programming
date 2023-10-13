fec6417be7eef5a1186d527ae1ad9cabfce86d81
bossambani <123232647+bossambani@users.noreply.github.com>
2023-10-13 16:05:15 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,15 @@
  */
 int _isupper(int c)
 {
-if (c >='A'&& c <='Z')
-{
-return (1);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-else 
-{
-return(0);
+
 }
-}
+
