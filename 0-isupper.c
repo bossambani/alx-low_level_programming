a7054d306124af086e24cdae26d4ac148fefd4ec
bossambani <bossambaka@gmail.com>
2023-10-13 15:57:37 +0300

    my commit

---
+++ 0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check if c is uppercase or not.
+ *
+ * Return 1 if c is uppercase.
+ */
+int _isupper(int c)
+{
+if (c >='A'&& c <='Z')
+{
+return (1);
+}
+else 
+{
+return(0);
+}
+}
