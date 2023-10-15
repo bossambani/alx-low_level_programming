d77e9cbc46569a026a3d039127992c6eef52d9a5
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-15 13:52:21 +0300

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**printing numbers from 0 to 10
+ * return 0
+ */
+int main(void)
+{
+        int i;
+        for (i = 0; i < 10; i++)
+        {
+                printf("%i",i)
+        }
+        putchar('\n')
+        return(0);
+}
