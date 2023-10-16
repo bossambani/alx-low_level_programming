1bb8a29e8893ae7930c18a56460bd05f70c91549
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-17 00:01:20 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,8 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /* HEADER_NAME_H */
