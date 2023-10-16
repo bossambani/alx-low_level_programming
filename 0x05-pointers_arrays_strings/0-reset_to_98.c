b283c28469edf9800293f06d3ad9e7a58ff44b48
bossambani <bossambaka@gmail.com>
2023-10-16 17:33:02 +0300

    Another commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * program to replace a value by 98 using pointers
+ *reset_to_98: program to replace a value by 98 using pointers
+ *n - variable to be used
  *Return: Always 0
  */
 void reset_to_98(int *n)
