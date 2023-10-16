a728d45f6d79fc29d77f82c98dc66598b339ce84
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-16 23:27:48 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,12 @@
  */
 void rev_string(char *s)
 {
- int length = strlen(s);
- int i, j;
- char temp;
+int length = strlen(s);
+int i, j;
+char temp;
 
-for (i = 0, j = length - 1; i < j; i++, j--) {
+for (i = 0, j = length - 1; i < j; i++, j--) 
+{
 temp = s[i];
 s[i] = s[j];
 s[j] = temp;
