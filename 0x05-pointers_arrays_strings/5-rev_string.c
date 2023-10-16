612a3efcc8d480b857b2d58fc0b97c3d023eae29
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-16 23:42:04 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,16 @@
  *@s: variable parameter
  *Return: Always(0).
  */
-void rev_string(char *s)
-{
-int length = strlen(s);
-int i, j;
-char temp;
-for (i = 0, j = length - 1; i < j; i++, j--) 
-{
-temp = s[i];
-s[i] = s[j];
-s[j] = temp;
+void reverseString(char *s) {
+    int length = strlen(s);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = length - 1; i < j; i++, j--) 
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
-}
+
