0b1082a1f3202e4e8162bf1bf90af898622c342a
bossambani <bossambaka@gmail.com>
2023-10-16 18:09:50 +0300

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - my function name
+ * @*str: variable name
+ * @len: variale name
+ * Return: Always (0).
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
