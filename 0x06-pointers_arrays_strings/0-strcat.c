ef7e91fa8dc4a9d4afa5eae0ac2a4e465b981185
bossambani <bossambaka@gmail.com>
2023-10-18 16:28:36 +0300

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest != 0)
+	{
+		dest++;
+	}
+	while (*src != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return result;
+}
