b047325956bb5c0a76218e3b65c8cdf5a1655d14
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-19 15:26:51 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,18 +3,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *result = dest;
-
-	while (*dest != 0)
-	{
-		dest++;
-	}
-	while (*src != 0)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return result;
+char *result = dest;
+while (*dest != 0)
+{
+dest++;
+}
+while (*src != 0)
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return result;
 }
