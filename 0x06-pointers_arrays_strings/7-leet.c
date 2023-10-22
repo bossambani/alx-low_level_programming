1783de8282329a75e1a1119c474d15ca9d560e29
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-22 13:24:25 +0300

    Update 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes string into 1337
+ * @n: input
+ * Return: n
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	char si[] = "aAeEoOtTlL";
+	char sj[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == si[j])
+			{
+				n[i] = sj[j];
+			}
+		}
+	}
+	return (n);
+}
