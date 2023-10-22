ade82128a35a02a2c8b368c51995dc7e1a003d58
bossambani <bossambaka@gmail.com>
2023-10-22 14:13:56 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - funtion that multiplies two numbers
+ *@a: input variable
+ *@b: input variable
+ *
+ */
+int mul(int a, int b)
+{
+	int result;
+	int result = a*b;
+
+	return (result);
+}
