a52ba775a164f70d67d7832381fb0d8a74b58065
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-22 13:31:16 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,7 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i])
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'
-))
+while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
 if (str[i - 1] == ' ' ||
 str[i - 1] == '\n' ||
