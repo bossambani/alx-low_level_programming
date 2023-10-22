2271cdffdb345b5abb909418030d1f97d3950163
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-22 13:53:03 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
  * main - checks for a digit value
- *
  * Return: 1 if c is a digit
  * Return: 0 otherwise
  */
 int _isdigit(int c)
 {
-        if (c >= 0 && c <= 9)
+        while (c >= 0 && c <= 9)
         {
                 return (1);
         }
-        else
-        {
                 return(0);
         }
 }
