988719d0db241887c85e2af3850133988cd9d1f5
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-22 13:51:18 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define HEADER_FILE
 
 int _isupper(int c);
-int _putchar(char c);
 int _isdigit(int c);
+int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
