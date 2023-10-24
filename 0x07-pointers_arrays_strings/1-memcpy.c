f7901dc60b694c1d37043140ad143ca011f36b1f
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-24 03:51:11 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,11 @@
  *@src: pointer variable
  *@n: bytes to e copied
  *@i: looping variable
- *Return: result.
+ *Return: dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-
 	while (i < n)
 	{
 		dest[i] = src[i];
