14879373ed6e5ca8ec13e34b1baaea4af77cce92
bossambani <bossambaka@gmail.com>
2023-10-26 04:38:02 +0300

    recurion commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+if (y == 1)
+{
+return (x);
+}
+return (x * _pow_recursion(x, y-1));
+}
