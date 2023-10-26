3385316131729ef8ed29c6b790d89440d8ec6fb9
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-26 05:40:58 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strlen - function to output length of a string
+ *_strlen_recursion - function to output length of a string
  *@s: string pointer
  *Return: length
  *
@@ -12,7 +12,7 @@ int length;
 length = 0;
 if (*s == '\0')
 return (0);
-length += _strlen_recursion(s+1);
+length += _strlen_recursion(s + 1);
 return (length);
 }
 
