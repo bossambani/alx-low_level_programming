57bddf3af89ff973f7461d9c97cbdb32e677b6b8
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-26 05:49:49 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,13 @@
  *_put_recursion - recursive function for printing out a string
  *@s: pointer to a string
  *Return: 0
- *
- *
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
