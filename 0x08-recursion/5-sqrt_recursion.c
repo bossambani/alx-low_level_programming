91c1d00902b5bc1850fc4fc8fff843a7003c8bf3
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-26 05:13:10 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int sqrt_calculation(int m, int i);
+int sqrt_calculation(int n, int i);
 /**
  *_sqrt_recursion - function to get the square root of a number
  *@n: integer variable
@@ -20,17 +20,17 @@ int _sqrt_recursion(int n)
  *Return: square root of a number
  *
  */
-int sqrt_calculation(int m, int i)
+int sqrt_calculation(int n, int i)
 {
 int sq;
 sq = i * i;
-if (sq == m)
+if (sq == n)
 {
 return (i);
 }
-if (sq < m)
+if (sq < n)
 {
-return (sqrt_calculation(m, i+1));
+return (sqrt_calculation(n, i+1));
 }
 return (-1);
 }
