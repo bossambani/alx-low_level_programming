7ab696e90b810bf72cd038c47661999127d59690
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-26 05:32:48 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@ int prime_calculation(int n, int i);
 /**
  *is_prime_number - function to compute prime number
  *@n: integer to be used
- *
+ **Return: 1 if n is prime otherwise return 0
  */
 int is_prime_number(int n)
 {
@@ -17,13 +17,13 @@ return (prime_calculation(n, n - 1));
  *prime_calculation - recursive function to calculate prime numbers
  *@n: variable to be calculated
  *@i: iteration variable
- *
+ *Return: 1 if n is prime otherwise return 0
  */
 int prime_calculation(int n, int i)
 {
 if (i == 1)
 return (1);
 if (n % i == 0 && i > 0)
-return(0);
+return (0);
 return (prime_calculation(n, i - 1));
 }
