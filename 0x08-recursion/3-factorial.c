99613472684a5581371b775c7ddce8874018da11
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-26 03:59:22 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
  *factorial - Function that compute factorial
  *@n: Function paramenter
  *Return: -1 when n is less than zero
- *otherwise Return 1 when n is zero or one
+ *otherwise Return 1 when n is zero 
  *otherwise Return the factorial of n
  */
 int factorial(int n)
@@ -12,13 +12,10 @@ if (n < 0)
 {
 return (-1);
 }
-else if (n <= 1)
+if (n == 0)
 {
 return (1);
 }
-else 
-{
 return n*factorial(n-1);
-}
 }
 
