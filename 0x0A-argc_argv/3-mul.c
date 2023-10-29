08ee5beb44770feb14e07f0d95df0ef8599ce3fe
bossambani <bossambaka@gmail.com>
2023-10-29 12:41:51 +0300

    my commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,20 @@ int main(int argc, char* argv[])
 {
 	int i;
 	int mult = 1;
-	if (argc == 3)
+	if (!(argc == 3))
 	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+
 		for (i =1; i < argc; i++)
 		{
 			mult *= atoi(argv[i]);
 			printf("%d\n", mult);
 		return (0);
-	}
-		else
-		{
-			printf ("Error\n");
-			return (1);
 		}
+	}
+		
 }
