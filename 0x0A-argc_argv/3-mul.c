006a2e8ff684a4ec458dfebb11f9502a0f8744c3
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-29 12:44:38 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
 	int i;
 	int mult = 1;
-	if (!(argc == 3))
+	if (argc != 3))
 	{
 		printf("Error\n");
 		return (1);
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
 	else
 	{
 
-		for (i =1; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			mult *= atoi(argv[i]);
 			printf("%d\n", mult);
