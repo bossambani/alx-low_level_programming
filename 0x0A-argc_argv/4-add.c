f470b67683b713d5a3c2a0596d209a7102e11a41
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-10-29 13:04:09 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,19 +12,13 @@ int check_numb(char *str)
 {
 /*Declaring variables*/
 unsigned int count;
-
-
 count = 0;
-while (count < strlen(str)) /*count string*/
-
-
+while (count < strlen(str)) 
 {
-if (!isdigit(str[count])) /*check if str there are digit*/
+if (!isdigit(str[count])) 
 {
 return (0);
 }
-
-
 count++;
 }
 return (1);
@@ -38,45 +32,26 @@ return (1);
  * Return: Always 0 (Success)
  */
 
-
 int main(int argc, char *argv[])
-
-
 {
-
-
-/*Declaring variables*/
 int count;
 int str_to_int;
 int sum = 0;
-
-
 count = 1;
-while (count < argc) /*Goes through the whole array*/
+while (count < argc)
 {
 if (check_numb(argv[count]))
-
-
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+str_to_int = atoi(argv[count]); 
 sum += str_to_int;
 }
-
-
-/*Condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
 return (1);
 }
-
-
 count++;
 }
-
-
-printf("%d\n", sum); /*print sum*/
-
-
+printf("%d\n", sum); 
 return (0);
 }
