db556ab901944c91473699b9bcf49fd0209714de
bossambani <bossambaka@gmail.com>
2023-11-01 14:11:32 +0000

    my commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ *_strdup - function that returns a pointer to a newly allocated space in memory
+ *@str: pointer
+ *Return: a.
+ */
+char *_strdup (char *str)
+{
+	char *a;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	a = strdup(str);
+	return (a);
+}
