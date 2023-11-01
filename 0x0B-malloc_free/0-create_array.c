4e327fd75e82eaa40ce64ca86b5cb0fb0dad0920
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-01 13:46:29 +0000

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,15 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	
 		return (NULL);
 	c = malloc(size * sizeof(*c));
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-	{
 		c[i] = c;
 		return (c);
-	}
-	
 }
