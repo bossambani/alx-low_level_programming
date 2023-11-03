7de8b92586ce77a26a5b86761d88699730835884
bossambani <bossambaka@gmail.com>
2023-11-03 05:12:14 +0300

    my commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: amount of bytes
+ *
+ * Return: pointer to the allocated memory.
+ * If malloc fails, status value is equal to 98
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int len1, len2, lsout, i;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (len1 = 0; s1[ls1] != '\0'; len1++)
+		;
+
+	for (len2 = 0; s2[ls2] != '\0'; len2++)
+		;
+
+	if (n > len2)
+		n = len2;
+
+	lsout = len1 + n;
+
+	s = malloc(lsout + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < len1)
+			s[i] = s1[i];
+		else
+			s[i] = s2[i - len1];
+	s[i] = '\0';
+
+	return (s);
+}
+
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_calloc - allocates memory to the array
+ *@nmemb: number of elements
+ *@size: size of elements
+ *return: Always 0;
+ */
+void *_calloc(unsigned int nmemb, unsigned int size) {
+  void *ptr;
+
+  if (nmemb == 0 || size == 0) {
+    return NULL;
+  }
+
+  ptr = malloc(nmemb * size);
+  if (ptr == NULL) {
+    return NULL;
+  }
+
+  memset(ptr, 0, nmemb * size);
+
+  return ptr;
+}
+
