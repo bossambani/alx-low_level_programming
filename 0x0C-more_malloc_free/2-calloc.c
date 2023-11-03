850f40b6dfffdfa46515dbc273197d58397c66d6
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-03 05:37:09 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,16 @@ void *_calloc(unsigned int nmemb, unsigned int size) {
   void *ptr;
 
   if (nmemb == 0 || size == 0) {
-    return NULL;
+    return (NULL);
   }
 
   ptr = malloc(nmemb * size);
   if (ptr == NULL) {
-    return NULL;
+    return (NULL);
   }
 
   memset(ptr, 0, nmemb * size);
 
-  return ptr;
+  return (ptr);
 }
 
