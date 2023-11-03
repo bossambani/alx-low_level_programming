b51ea5b626199d47f225787dbdc4173e4c40063c
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-03 05:32:44 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,10 +22,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[ls1] != '\0'; len1++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 
-	for (len2 = 0; s2[ls2] != '\0'; len2++)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
 	if (n > len2)
