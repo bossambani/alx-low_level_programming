d1a9aa71efdf54f963606c82c355a52c61ca6998
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-07 04:21:07 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,13 @@
  *@age: dog's age
  *@owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
-	char * owner;
-};
-typedef dog dog_t;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
