70604ba7e0b81051ef97dd2f659ff17bb8e2bb7b
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-07 04:23:04 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,16 +9,10 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		if (d->name == NULL)
-			printf("(nil)\n");
-		else
-			printf("%s\n," d->name);
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: ");
-		 if (d->owner == NULL)
-                        printf("(nil)\n");
-                else
-                        printf("%s\n," d->owner);
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
 
