03820567e42b66d1dd186a5a83f3a5bf90dbab6b
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-11 05:35:16 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,6 @@
  * @n: integers to sum
  * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
         unsigned int i;
@@ -13,11 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	if (n == 0)
-		return (0);
+	return (0);
 	va_start(args, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(args, int);
+	}
 	va_end(args);
 	return (sum);
 }
