a2010195c0d1ceca871827bb25b7955c317efa1f
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-11 05:32:40 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+        unsigned int i;
+	int sum;
+	va_list args;
+
 	if (n == 0)
 		return (0);
-	va_list args;
-	va_start (args, n);
-
-	for (int i = 0; i < n; i++)
-	{
+	va_start(args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	}
 	va_end(args);
 	return (sum);
 }
+	
