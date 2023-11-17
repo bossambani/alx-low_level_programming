93cf0b1656d65ef5aaa6ce4f26ae713b8479dd7f
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2023-11-17 05:24:29 +0300

    Update 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+* add_node - Adds a new node at the beginning of a list_t list
+* @head: A pointer to null address
+* @str: String to be printed
+* Return: Address of the new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
