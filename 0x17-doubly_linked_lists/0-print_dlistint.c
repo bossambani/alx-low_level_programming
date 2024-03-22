3f459ec145b2b064348bbf829d5c573e9620e3f4
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2024-03-22 23:25:31 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,25 @@
-#include <lists.h>
+#include "lists.h"
+/**
+ * print_dlistint - A function that prints elements of a list
+ * @h: pointer to header of the list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
+	const dlistint_t *temp;
+	size_t counter = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+	temp =  h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
 }
