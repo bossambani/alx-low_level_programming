6d25dbd7a12a978218684f58cc7fb939b147a4e3
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2024-04-01 06:14:39 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,13 @@
-#ifndef __MAIN_H_
-#define __MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
