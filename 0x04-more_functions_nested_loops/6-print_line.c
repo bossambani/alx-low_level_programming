f141fc6b7df9b1645f33085b63ec93f368b04439
bossambani <bossambaka@gmail.com>
2024-06-09 10:21:10 -0700

    updated 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * print_line - draw a straight line
- * @n: number of times underscore is printed
- * Description: Can only use _putchar to print
- */
+*print_line - draw a straight line
+*@n: number of times underscore is printed
+*Description: Can only use _putchar to print
+*/
 void print_line(int n)
 {
 	int c;
