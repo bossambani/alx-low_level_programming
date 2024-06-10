a6130787b406bde7e52990910218c962c1f307d0
bossambani <bossambaka@gmail.com>
2024-06-10 04:37:58 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,24 +4,24 @@
  *_strncat - Function for concatenating two strings
  *@dest: variable pointer
  *@src: variable pointer
- *@int n: bytes to be concatenated
+ *@n: bytes to be concatenated
  *Return: dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0;
-int j = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
