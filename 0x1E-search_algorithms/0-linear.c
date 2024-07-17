e87662723203fb83a2483e0b99ae443a2c548363
bossambani <bossambaka@gmail.com>
2024-07-17 06:54:54 -0700

    linear search updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,11 +9,11 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
