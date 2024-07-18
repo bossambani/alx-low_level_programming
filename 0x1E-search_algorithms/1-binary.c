facd9704c9526f39de048ff38ff381af063fef2d
bossambani <bossambaka@gmail.com>
2024-07-17 18:36:53 -0700

    Binary search updated

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,11 @@ int binary_search(int *array, size_t size, int value)
 	int i;
 	int mid;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (low <= high)
 	{
 		printf("Searching in array: ");
