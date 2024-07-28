347bf2a42a018e3b5bc097c57e25c83ec9bdf1e1
= <=>
2024-07-28 05:14:14 -0700

    palindromes check

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
- *
- *
+ *is_palindrome - a function that checks for palindromes
+ *@s: the string to be checked
+ *Return: 1 if palindrome else return 0
  */
 int is_palindrome(char *s)
 {
-	int left =0;
-	int right = strlen(s) -1;
+	int left = 0;
+	int right = strlen(s) - 1;
 
 	while (left < right)
 	{
-		if(s[left] != s[right])
+		if (s[left] != s[right])
 		{
 			return (0);
 		}
